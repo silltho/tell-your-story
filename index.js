a9dbf7d8f0eb67dfe37e56cc0fa29228adf1b655
Thomas Peter Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2016-10-29 20:43:05 +0200

    init sass

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(3333, function(){
+var sassMiddleware = require('node-sass-middleware');
+
+var srcPath = __dirname + '/sass';
+var destPath = __dirname + '/css';
+
+var app = connect();
+app.use('/styles', sassMiddleware({
+    /* Options */
+    src: srcPath,
+    dest: destPath,
+    debug: true,
+    outputStyle: 'expanded'
+}));
+
+app.use(serveStatic(__dirname));
+
+app.listen(3333, function(){
     console.log('Server running on Port 3333...');
 });
