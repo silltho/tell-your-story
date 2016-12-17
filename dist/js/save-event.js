b15e3abbede7d69bc9e856bbf9c9480cc8324d6c
Julia Reisinger <jreisinger.mmt-b2015@fh-salzburg.ac.at>
2016-12-17 21:15:52 +0100

    saving objects in localsession and get properties again

--- dist/js/save-event.js
+++ dist/js/save-event.js
@@ -1,24 +1,55 @@
-function save(){
-    var event_name=document.getElementById('event-name').value;
-    var event_description=document.getElementById('event-description').value;
-    var event_from=document.getElementById('event-from').value;
-    var event_to=document.getElementById('event-to').value;
+var eventNumber = 1;
 
-    var event = {'name': event_name, 'description': event_description, 'from': event_from, 'to': event_to};
+function createnewEvent(){
 
-    localStorage.setItem("event", JSON.stringify(event)); // save the item
+    var newEvent = new Object();
 
-    var testobject =localStorage.getItem("event"); // retrieve
-    console.log(JSON.parse(testobject));
+    newEvent .name = document.getElementById('event-name').value;
+    newEvent .description = document.getElementById('event-description').value;
+    newEvent .from = document.getElementById('event-from').value;
+    newEvent .to = document.getElementById('event-to').value;
+
+    if(sessionStorage.events)
+    {
+        events= JSON.parse(sessionStorage.getItem('events'));
+    }else{
+        events=[];
+    }
+    events.push(newEvent);
+   // console.log('retrievedObject: ', events);
+    sessionStorage.setItem('event', JSON.stringify(events));
+
+    // this is how you will retrive it
+
+
+    var retrievedObject = JSON.parse(sessionStorage.getItem('event'));
+
+    var result = getFields(retrievedObject, "description");
+    console.log(result);
+
+    //Gibt einzelne Properties als string zurück
+    function getEventProperties(input, field) {
+        var output = [];
+        for (var i=0; i < input.length ; ++i)
+            output.push(input[i][field]);
+        return output;
+    }
 
 
 
+  // console.log('aögain: ', retrievedObject);
+   // console.log('event-name: ', JSON.parse(retrievedObject));
+    eventNumber++;
 }
-
-
 
 function retrieve(){
 
+    console.log(JSON.parse(testobject['name']));
+    var testobject = localStorage.getItem("event");
+    document.getElementById('get-event-name').value = JSON.parse(testobject['name']);
+    document.getElementById('get-event-description').value = JSON.parse(testobject['description']);
+    document.getElementById('get-event-from').value = JSON.parse(testobject['from']);
+    document.getElementById('get-event-to').value = JSON.parse(testobject['to']);
 }
 
 
