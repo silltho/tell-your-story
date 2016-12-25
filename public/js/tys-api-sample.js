8947b8d3b2a5e077b772035ab2fa0cb538c24311
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2016-12-25 17:31:25 +0100

    refactor config file

---
+++ public/js/tys-api-sample.js
@@ -0,0 +1,7 @@
+var FIREBASE_CONFIG = {
+        apiKey: "firebaseApiKEey",
+        authDomain: "authDomain.firebaseapp.com",
+        databaseURL: "https://databaseName.firebaseio.com",
+        storageBucket: "storageBucket.appspot.com",
+        messagingSenderId: "senderId"
+};
--- public/js/tys-config.js
+++ public/js/tys-config.js
@@ -1,11 +1,3 @@
-var FIREBASE_CONFIG = {
-        apiKey: "AIzaSyBhMuIF7RjN2aYePvTtj8CWOC2JrZSfDFM",
-        authDomain: "mmp2-c6757.firebaseapp.com",
-        databaseURL: "https://mmp2-c6757.firebaseio.com",
-        storageBucket: "mmp2-c6757.appspot.com",
-        messagingSenderId: "1060144890044"
-};
-
 var LOCALSTORAGE_CONFIG = {
         eventsKey: 'events',
         eventsCounter: 'events-counter'
